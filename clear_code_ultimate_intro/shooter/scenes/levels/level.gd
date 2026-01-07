ead834fc104ccfc6614748d805bbbef79cc70276
Robbie Swager <swagerrobert@gmail.com>
2026-01-06 19:02:11 -0500

    Learned how to use code to load instances of scenes
    made a laser.tscn to store all the laser information
    
    add preload for new laser.tcsn in the level to load the assets
    
    instantiated a scene of the laser into a node and then added it to the level
    
    tied the summon location of the laser to the players position at the time it was summoned

--- clear_code_ultimate_intro/shooter/scenes/levels/level.gd
+++ clear_code_ultimate_intro/shooter/scenes/levels/level.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+# NOTE(rswager): STEP 1 - Before creating instances of scense we need to preload them
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+
 func _on_gate_player_entered_gate(body) -> void:
 	print(body.name, " body has entered Gate!")
 
@@ -7,7 +10,15 @@ func _on_gate_player_exited_gate(body: Variant) -> void:
 	print(body.name, " body has exited Gate!")
 
 func _on_player_laser() -> void:
-	print("<laser from level>")
+	# NOTE(rswager): STEP 2 - We need to create an instance/node
+	var laser = laser_scene.instantiate()
+	# 1. update the laser position
+	laser.position = $Player.position
+	# 2. we have to move the laser
+	# 3. Want to add the laser instance toa Node2D (keeps level scene clean)
+	
+	# NOTE(rswager): STEP 3 - We need to add the instance/node to the tree
+	add_child(laser)
 
 func _on_player_grenade() -> void:
 	print("<grenade from level>")
